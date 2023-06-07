7d7882d96026793167d89e5cebaf724c819cf7fd
Dinesh <dineshphuyel20@gmail.com>
2023-06-07 13:10:34 +0545

    added bradcrum, spatie permisson, bootstrap and jquery

---
+++ routes/breadcrumbs.php
@@ -0,0 +1,11 @@
+<?php
+
+use Diglactic\Breadcrumbs\Breadcrumbs;
+
+// This import is also not required, and you could replace `BreadcrumbTrail $trail`
+//  with `$trail`. This is nice for IDE type checking and completion.
+use Diglactic\Breadcrumbs\Generator as BreadcrumbTrail;
+
+Breadcrumbs::for('welcome', function (BreadcrumbTrail $trail) {
+    $trail->push('Welcome', route('welcome'));
+});
